97d91705a89a29ffcde4f7fe8c00200727d817c4
tuanhh <tuanhh@hybrid-technologies.vn>
2020-11-04 16:03:35 +0700

    commit auth

---
+++ resources/js/route.js
@@ -0,0 +1,66 @@
+import VueRouter from 'vue-router'
+
+// Pages
+import Home from './pages/home'
+import Register from './pages/register'
+import Login from './pages/login'
+import Users from './admin/User'
+// Routes
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: Home,
+        meta: {
+            auth: undefined
+        }
+    },
+    {
+        path: '/about',
+        name: 'about',
+        component: About,
+        meta: {
+            auth: undefined
+        }
+    },
+    {
+        path: '/register',
+        name: 'register',
+        component: Register,
+        meta: {
+            auth: false
+        }
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: Login,
+        meta: {
+            auth: false
+        }
+    },
+    // USER ROUTES
+    {
+        path: '/dashboard',
+        name: 'dashboard',
+        component: Dashboard,
+        meta: {
+            auth: true
+        }
+    },
+    // USER ROUTES
+    {
+        path: '/users',
+        name: 'users',
+        component: Users,
+        meta: {
+            auth: true
+        }
+    },
+]
+const router = new VueRouter({
+    history: true,
+    mode: 'history',
+    routes,
+})
+export default router
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,11 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('admin/home', 'HomeController@handleAdmin')->name('admin.route')->middleware('admin');
